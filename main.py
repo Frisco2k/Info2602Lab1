8dccad19953c4ab6fa2ae422201001524d75382e
Richard <richardleonard4505@gmail.com>
2025-01-28 23:17:45 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+
+global data
+
+# read data from file and store in global variable data
+with open('data.json') as f:
+        data = json.load(f)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!' # return 'Hello World' in response
+
+
+
+# route variables
+@app.route('/students')
+def get_students():
+  result = []
+  pref = request.args.get('pref') # get the parameter from url
+  if pref:
+    for student in data: # iterate dataset
+      if student['pref'] == pref: # select only the students with a given meal preference
+        result.append(student) # add match student to the result
+    return jsonify(result) # return filtered set if parameter is supplied
+  return jsonify(data) # return entire dataset if no parameter supplied
+
+
+    
+
+app.run(host='0.0.0.0', port=8080)

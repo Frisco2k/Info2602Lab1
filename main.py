fcec7f8e70e676d80b6a8415841519c87f635e22
Richard <richardleonard4505@gmail.com>
2025-01-30 00:47:20 -0400

    fixed code with exercises

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+# task 3
 from flask import Flask, request, jsonify
 import json
 
@@ -13,9 +15,7 @@ with open('data.json') as f:
 def hello_world():
     return 'Hello, World!' # return 'Hello World' in response
 
-
-
-# route variables
+#task 5
 @app.route('/students')
 def get_students():
   result = []
@@ -27,7 +27,53 @@ def get_students():
     return jsonify(result) # return filtered set if parameter is supplied
   return jsonify(data) # return entire dataset if no parameter supplied
 
-
+#task 4
+@app.route('/students/<id>')
+def get_student(id):
+  for student in data: 
+    if student['id'] == id: # filter out the students without the specified id
+      return jsonify(student)
     
+#exercise 1 
+@app.route('/stats')
+def get_stats():
+   stats = {
+      "Fish" : 0,
+      "Chicken" : 0,
+      "Vegetable" : 0,
+      "Computer Science (Major)" : 0,
+      "Computer Science (Special)" : 0,
+      "Information Technology (Major)" : 0,
+      "Information Technology (Special)" : 0,
+   }
+
+   for student in data:
+      if student['programme'] in stats:
+         stats[student['programme']] += 1 
+
+      if student['pref'] in stats:
+         stats[student['pref']] += 1
+    
+   return jsonify(stats)     
+
+#exercise 2
+@app.route('/add/<int:x>/<int:y>')
+def addition(x, y):
+   return jsonify (x + y)
+
+@app.route('/subtract/<int:x>/<int:y>')
+def subtraction(x, y):
+   return jsonify (x - y)
+
+@app.route('/multiply/<int:x>/<int:y>')
+def multiplication(x, y):
+   return jsonify (x * y)
+
+
+@app.route('/divide/<int:x>/<int:y>')
+def division(x, y):
+   return jsonify (x / y)
+
 
 app.run(host='0.0.0.0', port=8080)
+
